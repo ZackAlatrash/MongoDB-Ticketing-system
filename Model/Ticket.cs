82e3e1079443e8005b9a5dc92d4894dd49964c11
Zack Atrash <111879622+ZackAlatrash@users.noreply.github.com>
2025-04-01 23:07:27 +0200

    Add files via upload

---
+++ Model/Ticket.cs
@@ -0,0 +1,58 @@
+﻿using Model.Enums;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Model
+{
+    public class Ticket
+    {
+        [BsonId] // Marks this property as the ID field
+        public ObjectId Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ObjectId Reporter_id { get; set; }
+        public ObjectId Handler_id { get; set; }
+        public Enums.Status Status { get; set; }
+        public DateTime Start_Date { get; set; }
+        public DateTime End_Date { get; set; }
+        public Enums.Priority Priority { get; set; }
+        public Ticket(BsonDocument document)
+        {
+            this.Id = (ObjectId)document["_id"];
+            this.Title = (string)document["Title"];
+            this.Description = (string)document["Description"];
+            this.Reporter_id = (ObjectId)document["Reporter_id"];
+            this.Handler_id = (ObjectId)document["Handler_id"];
+            this.Status = (Status)(int)document["Status"];
+            this.Start_Date = (DateTime)document["Start_Date"];
+            this.End_Date = (DateTime)document["End_Date"];
+            this.Priority = (Priority)(int)document["Priority"];
+            //if (document["End_Date"] != null)
+            //{
+            //    this.End_Date = (DateTime)document["End_Date"];
+            //}
+        }
+
+        public Ticket(string title, string description, ObjectId reporter_id, Status status, DateTime start_Date, DateTime end_Date, Priority priority) 
+        { }
+        public Ticket(string title, string description, ObjectId reporter_id, ObjectId handler_id, Status status, DateTime start_Date, DateTime end_Date, Priority priority)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Reporter_id = reporter_id;
+            this.Handler_id = handler_id;
+            this.Status = status;
+            this.Start_Date = start_Date;
+            this.End_Date = end_Date;
+            this.Priority = priority;
+        }
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,74 @@
+﻿using Model.Enums;
+using MongoDB.Bson;
+
+public class User
+{
+
+
+    // Constructor to initialize a User object
+    public User(ObjectId id, string firstName, string lastName, string username, string password, string email, Roles role)
+    {
+        this.Id = id;
+        this.First_Name = firstName;
+        this.Last_Name = lastName;
+        this.Username = username;
+        this.Password = password;
+        this.Email = email;
+        this.Role = role;
+    }
+    public ObjectId Id { get; set; }
+    public string First_Name { get; set; }
+    public string Last_Name { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public Roles Role { get; set; }
+    public string Reset_Token { get; set; } 
+    public DateTime? Token_Expiration { get; set; } 
+
+        
+    
+        public User(BsonDocument document)
+        {
+            this.Id = (ObjectId)document["_id"];
+            this.First_Name = (string)document["first_name"];
+            this.Last_Name = (string)document["last_name"];
+            this.Username = (string)document["username"];
+            this.Password = (string)document["password"];
+            this.Email = (string)document["email"];
+            if (document["role"].IsString)
+            {
+                this.Role = (Roles)Enum.Parse(typeof(Roles), document["role"].AsString);
+            }
+            else if (document["role"].IsInt32)
+            {
+                this.Role = (Roles)document["role"].AsInt32;
+            }
+            if (document.Contains("reset_token"))
+            {
+                this.Reset_Token = document["reset_token"].ToString();
+            }
+            if (document.Contains("token_expiration"))
+            {
+                this.Token_Expiration = document["token_expiration"].ToNullableUniversalTime();
+            }
+        }
+        public override string ToString()
+        {
+            return this.Username;
+        }
+        public User() { }
+
+    // Constructor to handle BSONDocument from MongoDB
+/*    public User(BsonDocument document)
+    {
+        this.Id = (ObjectId)document["_id"];
+        this.First_Name = document.Contains("first_name") ? document["first_name"].AsString : "Unknown";
+        this.Last_Name = document.Contains("last_name") ? document["last_name"].AsString : "Unknown";
+        this.Username = document.Contains("username") ? document["username"].AsString : "Unknown";
+        this.Password = document.Contains("password") ? document["password"].AsString : string.Empty;
+        this.Email = document.Contains("email") ? document["email"].AsString : string.Empty;
+        this.Role = document.Contains("role") ? (Roles)(int)document["role"] : Roles.employee;
+    }*/
+
+}
